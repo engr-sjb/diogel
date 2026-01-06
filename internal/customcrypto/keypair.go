30f334f52d53e9f046a2b80f096e3c1e69c0cf52
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2026-01-06 14:02:58 +0000

    fix: resolve lack of send and receive chunk and add some other features.

--- internal/customcrypto/keypair.go
+++ internal/customcrypto/keypair.go
@@ -1,9 +1,20 @@
+/*
+	Copyright (c) 2025 Stephen Jersuit Benyah
+	Licensed under the Repo-Only Non-Commercial & No-Derivatives License with Anti-Training Clause (RONCND-AT) v1.0.
+	See LICENSE and CONTRIBUTION_LICENSE_AGREEMENT.md in repository root.
+	Prohibited: copying, reuse, redistribution, or use as training data for machine learning/AI.
+*/
+
 package customcrypto
 
 import (
 	"crypto/ed25519"
 	"crypto/rand"
 )
+
+type PublicKeyBytes []byte
+type PrivateKeyBytes []byte
+type PublicKeyStr string
 
 func generateKeyPair() (priv []byte, pub []byte, err error) {
 	pub, priv, err = ed25519.GenerateKey(rand.Reader)
--- internal/transport/remote_peer_conn.go
+++ internal/transport/remote_peer_conn.go
@@ -1,100 +1,175 @@
 package transport
 
 import (
-	"encoding/hex"
+	"errors"
+	"io"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 	"unsafe"
 
-	"github.com/engr-sjb/diogel/internal/features/ports"
+	"github.com/engr-sjb/diogel/internal/customcrypto"
 	"github.com/engr-sjb/diogel/internal/message"
 	"github.com/engr-sjb/diogel/internal/protocol"
 )
 
+const (
+	chunkSize = 256 * 1024
+)
+
+var (
+	ErrChunkSizeExceeded     = errors.New("chunk size exceeded")
+	ErrUnexpectedMessageType = errors.New("unexpected message type")
+)
+
 type remotePeerConn struct {
 	conn         net.Conn
-	publicKeyStr ports.PublicKey
-	publicKey    []byte
+	publicKeyStr customcrypto.PublicKeyStr
+	publicKey    customcrypto.PublicKeyBytes
 	protocol     protocol.Protocol
 
-	mu                          sync.RWMutex
-	isBeingRead, isBeingWritten bool
-	lastOp                      time.Time // lastOp holds the time when a last operation occurred.
+	writeMu     sync.Mutex
+	writeFrame  protocol.Frame
+	lastWriteOp atomic.Int64 // lastWriteOp holds the time when a last operation occurred.
+
+	readMu     sync.Mutex
+	readFrame  protocol.Frame
+	lastReadOp atomic.Int64 // lastReadOp holds the time when a last operation occurred.
+
+	// isBeingRead, isBeingWritten bool
 }
 
 // _ is compiler check to prevent silent errors when either RemotePeerConn or remotePeer don't hold same methods.
 var _ RemotePeerConn = (*remotePeerConn)(nil)
 
-func NewRemotePeer(publicKey []byte, conn net.Conn, proto protocol.Protocol) *remotePeerConn {
-	publicKeyStr := hex.EncodeToString(publicKey) // todo: maybe; use the unsafe package to convert to string for better performance. We wont be mutating the public key byte as it will be the underlying data for the string.
+func NewRemotePeer(
+	publicKey customcrypto.PublicKeyBytes,
+	conn net.Conn,
+	protocol protocol.Protocol) *remotePeerConn {
 	// NOTICE IMPORTANT: In order not to do an allocation and then copy just to get a string via hex.EncodeToString(publicKey) or string(publicKey) which is a performance overhead I don't want in this section. So we are using unsafe.String to get the pointer of the first element and then its length. I am doing this cause I know for a fact that there is no reason for the public bytes array or slice to be changed.
 	// NOTICE: The RISK 1: If the a byte or bytes of the underlying array or slice is changed, the string will be mutated. Which normal strings in Go don't do; they are immutable.
-	// NOTICE: The RISK 2: If the publicKey byte is a byte slice ([]byte) and we use append() on it for whatever reason, the underlying array (&publicKey[0]) pointers to will not point to the same pointer as before. We will have a dangling pointer. Meaning it will contain garbage data.
-	publicKeyStr := unsafe.String(&publicKey[0], len(publicKey))
+	// NOTICE: The RISK 2: If the publicKey byte is a byte slice ([]byte) and we use append() on it for whatever reason, the underlying array (unsafe.SliceData(publicKey)) pointers to will not point to the same pointer as before. We will have a dangling pointer. Meaning it will contain garbage data.
+	publicKeyStr := unsafe.String(unsafe.SliceData(publicKey), len(publicKey))
 
 	return &remotePeerConn{
 		conn:         conn,
-		publicKeyStr: ports.PublicKey(publicKeyStr),
+		publicKeyStr: customcrypto.PublicKeyStr(publicKeyStr),
 		publicKey:    publicKey,
 		protocol:     protocol,
-		lastOp:       time.Now(),
 	}
-}
-
-func (pr *remotePeerConn) Read(p []byte) (n int, err error) {
-	pr.mu.Lock()
-	pr.isBeingRead = !pr.isBeingRead
-	n, err = pr.conn.Read(p)
-	pr.isBeingRead = !pr.isBeingRead
-	pr.mu.Unlock()
-
-	return n, err
 }
 
 func (pr *remotePeerConn) Write(p []byte) (n int, err error) {
-	pr.mu.Lock()
-	pr.isBeingWritten = !pr.isBeingWritten
-	n, err = pr.conn.Write(p)
-	pr.isBeingWritten = !pr.isBeingWritten
-	pr.lastOp = time.Now()
-	pr.mu.Unlock()
+	pr.writeMu.Lock()
+	defer pr.writeMu.Unlock()
+	return pr.write(p)
+}
+
+func (pr *remotePeerConn) Read(p []byte) (n int, err error) {
+	pr.readMu.Lock()
+	defer pr.readMu.Unlock()
+
+	return pr.read(p)
+}
+
+func (pr *remotePeerConn) Send(msg message.Msg) error {
+	pr.writeMu.Lock()
+	defer pr.writeMu.Unlock()
+	return pr.send(msg)
+}
+
+func (pr *remotePeerConn) SendChunk(msg message.Msg, data []byte) (int, error) {
+	pr.writeMu.Lock()
+	defer pr.writeMu.Unlock()
+
+	if len(data) > chunkSize {
+		return 0, ErrChunkSizeExceeded
+	}
+
+	if err := pr.send(msg); err != nil {
+		return 0, err
+	}
+
+	return pr.write(data)
+}
+
+func (pr *remotePeerConn) ReceiveChunk(buf []byte) (message.Msg, int, error) {
+	pr.readMu.Lock()
+	defer pr.readMu.Unlock()
+
+	if err := pr.protocol.ReadFrame(pr, &pr.readFrame); err != nil {
+		return nil, 0, err
+	}
+
+	var size uint64 = 0
+
+	switch m := pr.readFrame.Payload.Msg.(type) {
+	case message.CapsuleStreamChuck:
+		size = m.Size
+
+	default:
+		return nil, 0, ErrUnexpectedMessageType
+	}
+
+	if size > chunkSize || int(size) > len(buf) {
+		return nil, 0, ErrChunkSizeExceeded
+	}
+
+	n, err := io.ReadFull(pr.conn, buf[:size])
+	if err == nil {
+		pr.lastReadOp.Store(time.Now().UnixNano())
+	}
+	return pr.readFrame.Payload.Msg, n, err
+}
+
+func (pr *remotePeerConn) write(p []byte) (int, error) {
+	n, err := pr.conn.Write(p)
+	if err == nil {
+		pr.lastWriteOp.Store(time.Now().UnixNano())
+	}
 
 	return n, err
 }
 
-func (pr *remotePeerConn) Send(msg message.Msg) error {
-	writeFrame := protocol.Frame{
-		Version: pr.protocol.Version(),
-		Payload: protocol.Payload{
-			Msg: msg,
-		},
-	}
-	return pr.protocol.WriteFrame(pr, &writeFrame)
+func (pr *remotePeerConn) send(msg message.Msg) error {
+	pr.writeFrame.Payload.Msg = msg
+	pr.writeFrame.Version = pr.protocol.Version()
+
+	return pr.protocol.WriteFrame(pr, &pr.writeFrame)
 }
 
-func (pr *remotePeerConn) PublicKeyStr() ports.PublicKey {
+func (pr *remotePeerConn) read(p []byte) (int, error) {
+	n, err := pr.conn.Read(p)
+	if err == nil {
+		pr.lastReadOp.Store(time.Now().UnixNano())
+	}
+
+	return n, err
+}
+
+func (pr *remotePeerConn) PublicKeyStr() customcrypto.PublicKeyStr {
 	return pr.publicKeyStr
 }
 
-func (pr *remotePeerConn) PublicKey() []byte {
+func (pr *remotePeerConn) PublicKey() customcrypto.PublicKeyBytes {
 	return pr.publicKey
 }
 
-func (pr *remotePeerConn) IsBeingRW() (isBeingRead bool, isBeingWritten bool) {
-	return pr.isBeingRead, pr.isBeingWritten
-}
-
 func (pr *remotePeerConn) IsStale(threshold time.Duration) bool {
-	defer pr.mu.RUnlock()
+	writeNano := pr.lastWriteOp.Load()
+	readNano := pr.lastReadOp.Load()
 
-	pr.mu.RLock()
-	if pr.lastOp.IsZero() {
-		// if time is zero value, we consider it as stale since its not been operated on
+	if writeNano == 0 && readNano == 0 {
 		return true
 	}
 
-	return time.Since(pr.lastOp) > threshold
+	mostRecentNano := writeNano
+	if readNano > writeNano {
+		mostRecentNano = readNano
+	}
+
+	return time.Since(time.Unix(0, mostRecentNano)) > threshold
 }
 
 func (pr *remotePeerConn) Close() error {
